aa55b8a2fb4e81f0993aad0bea2cb254f4870c9d
AtulDataWizard <wolfboyclover@gmail.com>
2024-01-24 18:43:35 +0530

    dynamic Typing

---
+++ 9_Dynamic_Typing.py
@@ -0,0 +1,10 @@
+#Python is highly flexible about reassigning variables to different types
+a=True
+print(a)
+a="Hello"
+print(a)
+a=1
+print(a)
+a=None
+print(a)
+#we call this dynamic typing since variable can change types readily
